54009bf1938e4c8c3f0a5b04ec30ab05a991b018
DasBo1975 <dasbo1975@outlook.de>
2025-09-29 23:13:33 +0200

    chore: fix ESLint flat config (mocha globals, ignore adapter-config.d.ts)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,43 @@
 // ioBroker eslint template configuration file for js and ts files
 // Please note that esm or react based modules need additional modules loaded.
 import config from '@iobroker/eslint-config';
+import globals from 'globals';
 
 export default [
     ...config,
+    {
+        // macht describe/it global bekannt
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'commonjs',
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+            },
+        },
+    },
     {
         // specify files to exclude from linting here
         ignores: [
             '.dev-server/',
             '.vscode/',
             '*.test.js',
+            '*.test.cjs',
             'test/**/*.js',
             '*.config.mjs',
             'build',
             'dist',
-            'admin/build', 
+            'admin/build',
             'admin/words.js',
             'admin/admin.d.ts',
             'admin/blockly.js',
             '**/adapter-config.d.ts',
+            'lib/adapter-config.d.ts',
         ],
     },
     {
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
-        // as this improves maintainability. jsdoc warnings will not block buiuld process.
+        // as this improves maintainability. jsdoc warnings will not block build process.
         rules: {
             // 'jsdoc/require-jsdoc': 'off',
             // 'jsdoc/require-param': 'off',
